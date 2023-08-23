65119ad6c0844f4de9605996c11b76faaf3916ad
Habid Enrique Manzur Restrepo <habibmanzur@outlook.com>
2023-08-22 21:00:00 -0500

    feat: enabled feature flag and activated container app as default

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_log_analytics_workspace" "this" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
 
 resource "azurerm_container_app_environment" "backend" {
