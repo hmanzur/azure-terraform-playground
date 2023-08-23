e998fc39dad71ffc40998e5bc5b2e54e6b835138
Habid Enrique Manzur Restrepo <habibmanzur@outlook.com>
2023-08-22 20:55:55 -0500

    feat: enabled feature flag and activated container app as default

--- main.tf
+++ main.tf
@@ -18,19 +18,22 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-resource "azurerm_service_plan" "backend" {
-  name                = "Backend-ServicePlan"
+# FRONTEND
+resource "azurerm_service_plan" "frontend" {
+  count               = var.enable_webapp ? 1 : 0
+  name                = "Frontend-ServicePlan"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "backend" {
+resource "azurerm_linux_web_app" "frontend" {
+  count               = var.enable_webapp ? 1 : 0
   name                = "habi-panda-webapp"
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_service_plan.backend.location
-  service_plan_id     = azurerm_service_plan.backend.id
+  location            = azurerm_service_plan.frontend[count.index].location
+  service_plan_id     = azurerm_service_plan.frontend[count.index].id
 
   site_config {
     application_stack {
@@ -38,3 +41,36 @@ resource "azurerm_linux_web_app" "backend" {
     }
   }
 }
+# END FRONTEND
+
+# CONTAINER
+resource "azurerm_container_app_environment" "backend" {
+  count               = var.enable_containerapp ? 1 : 0
+  name                = "Backend-Environment"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_container_app" "backend" {
+  count                        = var.enable_containerapp ? 1 : 0
+  name                         = "example-app"
+  container_app_environment_id = azurerm_container_app_environment.backend[count.index].id
+  resource_group_name          = azurerm_resource_group.main.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "backend"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].container[0].image
+    ]
+  }
+}
+# END CONTAINER
--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "owner" {
   type    = string
   default = "habi-panda"
 }
+
+variable "enable_webapp" {
+  type        = bool
+  default     = false
+  description = "Create frontend application"
+}
+
+variable "enable_containerapp" {
+  type        = bool
+  default     = true
+  description = "Create containered application"
+}
